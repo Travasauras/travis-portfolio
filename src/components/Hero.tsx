af9c9eafed493d22f9dedf005de82e146eaa29f3
unknown <travis.erwin@snhu.edu>
2026-07-25 12:48:16 -0500

    Created reusable Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">Hi, I&apos;m Travis 👋</h1>
+
+      <p className="mt-4 text-xl text-gray-600">
+        Software Engineer • Computer Science Student • Game Developer
+      </p>
+    </main>
+  );
+}
